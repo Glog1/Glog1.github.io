0dd997ce829ff057419e14d8af3ad68327460370
Glog1.github.io <serhiybilik@gmail.com>
2017-10-12 13:27:16 +0300

    1

---
+++ testajax/js/common.js
@@ -0,0 +1,44 @@
+$(function(){
+$(document).pjax('a[data-pjax]', '#pjax-container')
+    //   $(document).pjax('.ajaxLink','#ajaxContainer',{fragment: '#ajaxContainer'}); 
+});
+
+jQuery(document).ready(function () {
+    $(function ()
+    {
+        $('.navBar-mnu').bind(
+                'jsp-scroll-y',
+                function (event, scrollPositionY, isAtTop, isAtBottom)
+                {
+                    console.log('Handle jsp-scroll-y', this,
+                            'scrollPositionY=', scrollPositionY,
+                            'isAtTop=', isAtTop,
+                            'isAtBottom=', isAtBottom);
+                }
+        )
+                .jScrollPane();
+    });
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+
+
+    $('#btnMnu').click(function () {
+        $(this).toggleClass('open');
+    });
+
+    $('#btnMnu').click(function () {
+        $('#navBar').toggleClass('navBarOpen');
+    });
+    $('#btnHeader').click(function () {
+          $(this).toggleClass('open');
+        $('#header').toggleClass('headerOpen');
+    });
+
+});
