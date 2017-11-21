12e380dfc729b5b099fa0b23fd1d6053043cd887
Glog1.github.io <serhiybilik@gmail.com>
2017-11-21 17:49:02 +0200

    first commit

---
+++ nails/js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+//Masonry
+    var $container = $(".grid");
+
+        $container.masonry({
+            columnWidth: ".grid-item",
+            itemSelector: ".grid-item"
+        });
+
+});
+
